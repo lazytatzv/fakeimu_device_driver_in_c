414c8ad84fef31c36952d5d3f9a1c22788d2dd23
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-05-23 11:48:26 +0900

    rewrote simpelchar to emit event to udev

--- simplechar.c
+++ simplechar.c
@@ -1,11 +1,16 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
 #include <linux/uaccess.h>
 
 #define DEVICE_NAME "simplechar"
 
-static int major;
+static dev_t dev_num;
+static struct cdev simple_cdev;
+static struct class *simple_class;
+
 static char msg[] = "hello, from kernel!\n";
 
 static int dev_open(struct inode *inode, struct file *file) {
@@ -39,14 +44,47 @@ static struct file_operations fops = {
 };
 
 static int __init simple_init(void) {
-  major = register_chrdev(0, DEVICE_NAME, &fops);
-  printk(KERN_INFO "Successfully Initialized!\n");
-  printk(KERN_INFO "major = %d\n", major);
+  int ret;
+  // major = register_chrdev()
+  ret = alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME);
+
+  if (ret < 0)
+    return ret;
+  
+  printk(KERN_INFO "major = %d, minor = %d\n", MAJOR(dev_num), MINOR(dev_num));
+
+  cdev_init(&simple_cdev, &fops);
+  
+  ret = cdev_add(&simple_cdev, dev_num, 1);
+
+  if (ret < 0)
+    return ret;
+
+  simple_class = class_create(DEVICE_NAME);
+
+  if (IS_ERR(simple_class))
+    return PTR_ERR(simple_class);
+  
+  device_create(
+    simple_class,
+    NULL,
+    dev_num,
+    NULL,
+    DEVICE_NAME);
+
+  printk(KERN_INFO "Successfully Initialized!");
+
   return 0;
 }
 
 static void __exit simple_exit(void) {
-  unregister_chrdev(major, DEVICE_NAME);
+  device_destroy(simple_class, dev_num);
+  class_destroy(simple_class);
+  cdev_del(&simple_cdev);
+  unregister_chrdev_region(dev_num, 1);
+
+
+  //unregister_chrdev(major, DEVICE_NAME);
   printk(KERN_INFO "unloaded!");
 }
 
@@ -54,3 +92,4 @@ module_init(simple_init);
 module_exit(simple_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Modern version of simplechar");
